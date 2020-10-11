827c6dd8177f8196cb9d54526b22199f2a479800
sskwodgik <ssk@wodgik.katowice.pl>
2020-10-11 20:32:45 +0200

    pierwszy element tablicy

---
+++ main.js
@@ -0,0 +1,8 @@
+let tablica = [143, 3, 5, 7, 9];
+
+function pierwszyElement(tablica){
+    let pierwszyElement = tablica[0];
+    return pierwszyElement;
+}
+
+console.log(pierwszyElement(tablica));
